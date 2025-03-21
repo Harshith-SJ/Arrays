25f99d4c15880d2b05cd517bc1c7c9c9a2720089
Harshith S J <144507417+Harshith-SJ@users.noreply.github.com>
2025-03-21 21:09:22 +0530

    twosum using hashmap

---
+++ TwoSum/hashmap.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // 1st key 2nd index
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(nums[0],0);
+        for(int i=1;i<nums.length;i++){  
+           int diff=target-nums[i];
+           if(map.containsKey(diff)){
+            return new int[]{ map.get(diff),i };
+           }
+           else{
+            map.put(nums[i],i);
+           }
+        }
+    return null;
+    }
+}
